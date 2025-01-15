d56335e478ddafa5cc67c25aad20d24030a0a178
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-01-14 22:14:18 -0500

    Create tree class. add build_tree,pretty_print

---
+++ lib/tree.rb
@@ -0,0 +1,30 @@
+require "rubocop"
+require_relative "node"
+
+class Tree
+  def initialize(array)
+    @root = nil
+    @arr = array.uniq.sort
+  end
+
+  def build_tree(array = nil, start = nil, final = nil)
+    array = @arr if array.nil?
+    start = 0 if start.nil?
+    final = array.length - 1 if final.nil?
+    mid = (start + final) / 2
+
+    return if start > final
+
+    root = Node.new(mid)
+    root.left_children = build_tree(array, start, mid - 1)
+    root.right_children = build_tree(array, mid + 1, final)
+
+    @root = root
+  end
+
+  def pretty_print(node = @root, prefix = "", is_left = true)
+    pretty_print(node.right_children, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_children
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data_attribute}"
+    pretty_print(node.left_children, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_children
+  end
+end
